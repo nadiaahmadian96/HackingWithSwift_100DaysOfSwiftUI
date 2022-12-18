22cc7656bff4da80bd559c00ebb2bbec4e1f7bfe
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2022-12-18 11:23:50 +0330

    Create CheckPoint2.swift
    This time the challenge is to create an array of strings, then write some code that prints the number of items in the array and also the number of unique items in the array.

---
+++ CheckPoint2.swift
@@ -0,0 +1,4 @@
+let albums = ["Ghostlight","Carnival of Rust", "Ultraviolet","Signs of Life", "Temple of Thought", "Temple of Thought"]
+
+var albumsCount = albums.count
+var uniqueItemsCount = Set(albums).count
