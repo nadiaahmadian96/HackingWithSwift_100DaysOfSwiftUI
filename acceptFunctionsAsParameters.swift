9eae2019d8cf600e28097f5486ff7b93e808d2d9
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2022-12-24 23:15:28 +0330

    Create acceptFunctionsAsParameters.swift

---
+++ acceptFunctionsAsParameters.swift
@@ -0,0 +1,46 @@
+//Here’s a function that generates an array of integers by repeating a function a certain number of times:
+
+func makeArray(size: Int, using generator : ()->Int ) -> [Int]{
+    var numbers = [Int]()
+    
+    for _ in 0...size{
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+    
+    return numbers
+}
+
+let rolls = makeArray(size: 50) {
+    Int.random(in: 1...20)
+}
+print(rolls)
+
+
+func generateNumber() -> Int {
+    Int.random(in: 1...20)
+}
+
+let newRolls = makeArray(size: 50, using: generateNumber)
+print(newRolls)
+
+// here’s a function that accepts three function parameters, each of which accept no parameters and return nothing:
+
+func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start first work")
+    first()
+    print("About to start second work")
+    second()
+    print("About to start third work")
+    third()
+    print("Done!")
+}
+
+doImportantWork {
+    print("This is the first work")
+} second: {
+    print("This is the second work")
+} third: {
+    print("This is the third work")
+}
+
