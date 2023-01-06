b855f5865a06a887bdcf3be5aea8ae13c1a54354
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2023-01-06 13:24:24 +0330

    Create SplitterApp.swift

---
+++ Split/Splitter/SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitterApp.swift
+//  Splitter
+//
+//  Created by Nadia Ahmadian on 2023-01-03.
+//
+
+import SwiftUI
+
+@main
+struct SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
