aac3115c2b9d6e83938d826e94dd65e0992b54e0
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2023-01-10 14:54:05 +0330

    Create colors.swift

---
+++ colors.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+
+            Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+        }
+        .ignoresSafeArea()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
